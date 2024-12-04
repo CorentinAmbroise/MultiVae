747951e517545b01855f99b4a8843808cd8c38e8
CorentinAmbroise <corentin.ambroise9132@gmail.com>
2024-12-04 16:40:30 +0100

    added test with and without transform

--- tests/test_multimodal_dataset.py
+++ tests/test_multimodal_dataset.py
@@ -8,14 +8,20 @@ from multivae.data.datasets.base import MultimodalBaseDataset
 
 
 class Test:
-    @pytest.fixture
-    def input_dataset_test(self):
+    @pytest.fixture(params=["no_transform", "transform"])
+    def input_dataset_test(self, request):
         data = dict(
             mod1=np.array([[1, 2], [4, 5]]),
             mod2=np.array([[67, 2, 3], [1, 2, 3]]),
         )
         labels = np.array([0, 1])
-        return dict(data=data, labels=labels)
+        transform = None
+        if request.param == "transform":
+            transform = dict(
+                mod1=lambda x: x,
+                mod2=lambda x: -x,
+            )
+        return dict(data=data, labels=labels, transform=transform)
 
     def test_create_dataset(self, input_dataset_test):
         dataset = MultimodalBaseDataset(**input_dataset_test)
@@ -23,5 +29,8 @@ class Test:
         sample = dataset[0]
         assert type(sample) == DatasetOutput
         assert np.all(sample["data"]["mod1"] == np.array([1, 2]))
-        assert np.all(sample["data"]["mod2"] == np.array([67, 2, 3]))
+        if dataset.transform is None:
+            assert np.all(sample["data"]["mod2"] == np.array([67, 2, 3]))
+        else:
+            assert np.all(sample["data"]["mod2"] == -np.array([67, 2, 3]))
         assert sample["labels"] == 0
